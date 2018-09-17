cc533137cd0f45cff28506e5d7d894c809025662
BT_1N3_09@BKTECH.LOCAL <BT_1N3_09@BKTECH.LOCAL>
2018-09-17 10:21:07 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main
+{
+    public class MShahiduzzamanLib
+    {
+        public class TestFile
+        {
+            public static void main(String[] args)
+            {
+                public static isPalendrome(String word)
+                {
+
+                }
+            }
+        }
+    }
+}
